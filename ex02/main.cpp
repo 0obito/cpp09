888d77eb12e389c68eb34d66faf105a951ed074d
0obito <elmsafer.abdellah00@gmail.com>
2026-08-01 16:45:01 +0100

    Init commit. Reading numbers into a container is done

---
+++ ex02/main.cpp
@@ -0,0 +1,66 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <cerrno>
+#include <climits>
+#include "PmergeMe.hpp"
+
+int main(int ac, char* av[]) {
+    if (ac <= 1) {
+        std::cerr << "Error: 0 elements were provided.\n";
+        return 1;
+    }
+    if (ac == 2) {
+        std::cerr << "Error: can't sort 1 element.\n";
+        return 1;
+    }
+
+    size_t numOfElements = ac - 1;
+    std::cout<<"number of elements passed: "<<numOfElements<<std::endl;
+    std::vector<int> intVec;
+    intVec.reserve(numOfElements);
+
+    char* endPtr;
+    errno = 0;
+    for (size_t i = 1; i <= numOfElements; i++) {
+        long longElement = std::strtol(av[i], &endPtr, 10);
+        if (errno == ERANGE) {
+            if (longElement == LONG_MAX) {
+                std::cerr << "Error: element " << i << " is too large (Overflow).\n";
+            }
+            else if (longElement == LONG_MIN) {
+                std::cerr << "Error: element " << i << " is too small (Underflow).\n";
+            }
+            else {
+                std::cerr << "Error: range issue occured.\n";
+            }
+            return 1;
+        }
+        if (av[i] == endPtr) {
+            std::cerr << "Error: at element " << i << ", couldn't parse any digits.\n";
+            return 1;
+        }
+        if (*endPtr != '\0') {
+            std::cerr << "Error: at element " << i << ", trailing invalid characters.\n";
+            return 1;
+        }
+        if (longElement > INT_MAX) {
+            std::cerr << "Error: element " << i << " is too large (Overflow).\n";
+            return 1;
+        }
+        if (longElement < INT_MIN) {
+            std::cerr << "Error: element " << i << " is too small (Underflow).\n";
+            return 1;
+        }
+        int intElement = static_cast<int>(longElement);
+        intVec.push_back(intElement);
+    }
+
+    // print vector
+    std::vector<int>::iterator it = intVec.begin();
+    for (;it < intVec.end(); it++) {
+        std::cout << *it << "\n";
+    }
+
+    return 0;
+}
